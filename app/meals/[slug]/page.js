a1695a1da7e9d24b6cb437d599d678b68f3356f7
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-04-10 16:23:36 +0100

    inital-commit

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function meal({ params }) {
+
+return (
+    <h1>{params.slug}</h1>
+)
+
+}
---
+++ app/meals/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export default function Meals(){
+
+    return (
+        <>
+            <h1>Meals</h1>   
+            <Link href="meals/share">Share</Link>
+        </>
+    )
+}
