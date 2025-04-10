0f41c0d81d151f07118aa9043dd9844993137a4b
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-04-10 16:23:36 +0100

    inital-commit

---
+++ app/community/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function CommunityPage() {
+
+    return (
+       <>
+        <h1>community</h1>
+        <Link href="/">Home</Link>
+       </>
+    )
+
+}
