1620c93598c4ed9da22eb1812d75e61b616a67fb
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-05-04 23:00:37 +0100

    update

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,11 @@ import Link from "next/link";
 
 import classes from "./page.module.css"
 import MealsGrid from "@/components/meals-grid.js/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function Meals(){
+export default async function Meals(){
+
+    const meals = await getMeals();
 
     return (
         <>
@@ -22,7 +25,7 @@ export default function Meals(){
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={[]}/>
+                <MealsGrid meals={meals}/>
             </main>
         </>
     )
