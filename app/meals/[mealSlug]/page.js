0155d2e2477bbe3b0721c635575456aa36bbc40e
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-05-05 16:20:49 +0100

    updates

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,32 @@
+import { getMeal } from "@/lib/meals"
+import classes from "./page.module.css"
+import Image from "next/image"
+
+export default function meal({ params }) {
+
+    const meal = getMeal(params.mealSlug)
+
+    return (
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${"EMAIL"}`}>NAME</a>
+                    </p>
+                    <p className={classes.summary}>SUMMARY</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: "...",
+                }}></p>
+
+            </main>
+        </>
+    )
+
+}
