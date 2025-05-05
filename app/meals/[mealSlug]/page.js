c51bb54ebc7ea28fd9b87aa0ee852e3928afed65
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-05-05 22:47:08 +0100

    update

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,29 +1,39 @@
+import { notFound } from "next/navigation";
 import { getMeal } from "@/lib/meals"
+
 import classes from "./page.module.css"
 import Image from "next/image"
 
-export default function meal({ params }) {
+export default function MealPage({ params }) {
 
-    const meal = getMeal(params.mealSlug)
+    const meal = getMeal(params.mealSlug);
+
+    if (!meal){
+        notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, "<br />")
 
     return (
         <>
             <header className={classes.header}>
                 <div className={classes.image}>
-                    <Image fill />
+                    <Image src={meal.image} alt={meal.title} fill />
                 </div>
                 <div className={classes.headerText}>
                     <h1>{meal.title}</h1>
                     <p className={classes.creator}>
-                        by <a href={`mailto:${"EMAIL"}`}>NAME</a>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
                     </p>
-                    <p className={classes.summary}>SUMMARY</p>
+                    <p className={classes.summary}>{meal.summary}</p>
                 </div>
             </header>
             <main>
-                <p className={classes.instructions} dangerouslySetInnerHTML={{
-                    __html: "...",
-                }}></p>
+                <p 
+                    className={classes.instructions} dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }}>
+                </p>
 
             </main>
         </>
