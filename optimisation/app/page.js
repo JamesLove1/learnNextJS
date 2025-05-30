0b9151127710b05a13f0262363c1e2b35c0b9700
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-05-30 13:18:30 +0100

    updates

---
+++ optimisation/app/page.js
@@ -0,0 +1,28 @@
+import { Suspense } from 'react';
+
+import Posts from '@/components/posts';
+import { getPosts } from '@/lib/posts';
+
+async function LatestPosts() {
+  const latestPosts = await getPosts(2);
+  return <Posts posts={latestPosts} />;
+}
+
+export const metadata = {
+  title: "Latest Posts",
+  description: "Brows our latest posts"
+}
+
+export default async function Home() {
+  return (
+    <>
+      <h1>Welcome back!</h1>
+      <p>Here's what you might've missed.</p>
+      <section id="latest-posts">
+      <Suspense fallback={<p>Loading recent posts...</p>}>
+        <LatestPosts />
+      </Suspense>
+      </section>
+    </>
+  );
+}
